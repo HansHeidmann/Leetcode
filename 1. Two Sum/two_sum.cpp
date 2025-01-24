0bd431535dfb606f380128705978208ccf7015d9
HansHeidmann <hansheidmann@gmail.com>
2025-01-23 21:36:50 -0800

    leet

--- 1. Two Sum/two_sum.cpp
+++ 1. Two Sum/two_sum.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int i=0; i<nums.size(); i++) {
+            hashmap[nums[i]] = i; 
+        }
+        for (int i=0; i<nums.size(); i++) {
+            int difference = target-nums[i];
+            if (hashmap.find(difference) != hashmap.end()) {
+                if (hashmap[difference] != i) {
+                    return {hashmap[difference], i};
+                }
+            }
+        }
+        return {};
+    }
+};
