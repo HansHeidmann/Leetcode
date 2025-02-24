761bc00826bc39cb50ef65eec6703a76f07d553b
HansHeidmann <hansheidmann@gmail.com>
2025-02-23 20:39:07 -0800

    #290 py

---
+++ 290. Word Pattern/word_pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        words_dict = {}
+        letters_dict = {}
+        for i in range(0, len(pattern)):
+            if not pattern[i] in letters_dict:
+                letters_dict[pattern[i]] = words[i]
+                words_dict[words[i]] = pattern[i]
+            else: # if the letter has been used
+                
+        print(words_dict)
+        return True
+
+        
